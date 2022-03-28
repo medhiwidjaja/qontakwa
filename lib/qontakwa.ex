42c8728dd293b61c498ef267d607b501fc2e861d
Medhi Widjaja <medhiwidjaja@yahoo.com>
2022-03-28 17:06:51 +0700

    Qontakwa public API

--- lib/qontakwa.ex
+++ lib/qontakwa.ex
@@ -2,17 +2,37 @@ defmodule Qontakwa do
   @moduledoc """
   Documentation for `Qontakwa`.
   """
+  alias Qontakwa.WA
+
+  @body [
+    %{
+      key: "1",
+      value: "full_name",
+      value_text: "William Darcy"
+    },
+    %{
+      key: "2",
+      value: "pin",
+      value_text: "123445"
+    }
+  ]
 
   @doc """
-  Hello world.
+  Send WA message to specified phone number and name.
+  The body of message will use a default value, if not provided
 
-  ## Examples
-
-      iex> Qontakwa.hello()
-      :world
-
+  Example
+    iex> Qontakwa.send_wa_message "62818000111222", "Joko"
+    {:ok,
+    %Mojito.Response{
+      body: "{...}",
+      complete: true,
+      headers: [...],
+      size: 1111,
+      status_code: 201
+    }
   """
-  def hello do
-    :world
+  def send_wa_message(to_phone, to_name, body \\ @body) do
+    WA.send(to_phone, to_name, body)
   end
 end
