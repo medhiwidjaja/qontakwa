36a724ed17a16d5e2311588d6eed5dadf26edef6
Medhi Widjaja <medhiwidjaja@yahoo.com>
2022-03-28 17:06:36 +0700

    WA send API and test suite

---
+++ lib/wa.ex
@@ -0,0 +1,48 @@
+defmodule Qontakwa.WA do
+  @moduledoc """
+  This module interface with Qontak API to send Whatsapp message through their API
+  """
+
+  @base_url "https://chat-service.qontak.com/api/open/v1/broadcasts/whatsapp/direct"
+  @message_template_id "f9c092ba-9e33-427a-af31-6a91d5ec084d"
+  @channel_integration_id "22a75949-446a-4267-8ac2-1f105441d8fc"
+
+  @doc """
+  Send WA message through Qontak API.
+
+  ## Examples
+
+      iex> Qontakwa.send_wa_message(body)
+      {:ok, response}
+
+  """
+  def send(to_number, to_name, body, opts \\ []) do
+    headers = [
+      {"content-type", "application/json"},
+      {"authorization", "Bearer #{System.fetch_env!("QONTAK_TOKEN")}"}
+    ]
+
+    url = opts[:url] || @base_url
+    message_template_id = opts[:message_template_id] || @message_template_id
+    channel_integration_id = opts[:channel_integration_id] || @channel_integration_id
+
+    payload = %{
+      to_number: to_number,
+      to_name: to_name,
+      pin: "pin",
+      message_template_id: message_template_id,
+      channel_integration_id: channel_integration_id,
+      language: %{
+        code: "id"
+      },
+      parameters: %{
+        body: body
+      }
+    }
+
+    IO.inspect(url, label: "URL")
+    IO.inspect(headers, label: "HEADERS")
+
+    Mojito.post(url, headers, Jason.encode!(payload))
+  end
+end
--- test/qontakwa_test.exs
+++ test/qontakwa_test.exs
@@ -1,8 +1,31 @@
-defmodule QontakwaTest do
+defmodule Qontakwa.WATest do
   use ExUnit.Case
-  doctest Qontakwa
+  alias Qontakwa.WA
 
-  test "greets the world" do
-    assert Qontakwa.hello() == :world
+  setup do
+    bypass = Bypass.open()
+    {:ok, bypass: bypass}
+  end
+
+  describe "sending WA message" do
+    test "send valid request", %{bypass: bypass} do
+      Bypass.expect(bypass, fn conn ->
+        Plug.Conn.resp(conn, 201, payload())
+      end)
+
+      url = "http://localhost:#{bypass.port}/"
+      body = %{key: "value"}
+
+      response = WA.send("62812000111333", "Bob", body, url: url)
+
+      assert {:ok, %{status_code: 201}} = response
+    end
+  end
+
+  defp payload do
+    ~s({
+      "complete": "true",
+      "status_code": "201"
+    })
   end
 end
